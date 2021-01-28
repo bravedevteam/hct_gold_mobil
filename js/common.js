813f2a0b7d2b4a8e32144d7f105e115f4311a443
TeamBrave_Dev <73458930+bravedevteam@users.noreply.github.com>
2021-01-28 17:55:41 +0900

    JS 업데이트

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 $(function(){
+	/* Checkbox */
 	$(".areaAllCheck input[type='checkbox']").click(function(){
 		if($(this).prop("checked") == false){
 			$(this).parents(".boxBasicAgree").find(".item").children("input[type='checkbox']").prop("checked", "");
@@ -19,4 +20,63 @@ $(function(){
 			$parents.find(".areaAllCheck").children("input[type='checkbox']").prop("checked", "");
 		}
 	});
+	
+
+	/* Filebox */
+	var fileTarget = $('.filebox .uploadHidden');
+
+	fileTarget.on('change', function(){ // 값이 변경되면
+		if(window.FileReader){ // modern browser
+			var filename = $(this)[0].files[0].name;
+		}else{ // old IE
+			var filename = $(this).val().split('/').pop().split('\\').pop(); // 파일명만 추출
+		} // 추출한 파일명 삽입
+		$(this).siblings('.uploadName').val(filename);
+	});
+
+
+	/* tabBasic */
+	$(".tabBasic button").click(function(){
+		if(!$(this).hasClass("active")){
+			$(this).addClass("active");
+			$(this).siblings().removeClass("active");
+		}
+	});
+	
+
+	/* Popup Layer */
+	$(".layer--open").click(function(e){
+		e.preventDefault();
+		var target = $(this).attr("data-pop");
+
+		$("#"+target).addClass("active");
+		dim_open();
+	});
+
+	$(".layer--close").click(function(){
+		dim_close();
+	});
+
+
+	/* Dim */
+	$("#dim").click(function(){
+		dim_close();
+	});
+	function dim_open(){
+		$("body").css("overflow", "hidden");
+		$("#dim").fadeIn(500);
+	}
+	function dim_close(){
+		$("body").css("overflow", "auto");
+		$(".bottomLayer").removeClass("active");
+		$(".textLayer").removeClass("active");
+		$("#dim").fadeOut(500);
+	}
 });
+
+/* Number MaxLength */
+function maxLengthCheck(object){
+	if (object.value.length > object.maxLength){
+		object.value = object.value.slice(0, object.maxLength);
+	}    
+}
