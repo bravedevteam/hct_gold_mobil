238dfd17cdf2a6427a31cc9714ac4df1023a3037
TeamBrave_Dev <73458930+bravedevteam@users.noreply.github.com>
2021-03-23 12:08:37 +0900

    Add files via upload

--- admin/js/common.js
+++ admin/js/common.js
@@ -1,11 +1,4 @@
 $(function(){
-	$(".topArea_tab a").click(function(e){
-		e.preventDefault();
-		var target = $(this).attr("data-role");
-		if(!$(this).hasClass("active")){
-			$(this).addClass("active").siblings().removeClass("active");
-			$(".tabArea").removeClass("active");
-			$("#"+target).addClass("active");
-		}
-	});
+
 });
+
