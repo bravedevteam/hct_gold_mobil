f839b13f7377440a52dd8af33afe38f437ef5852
TeamBrave_Dev <73458930+bravedevteam@users.noreply.github.com>
2021-04-19 15:56:44 +0900

    Add files via upload

--- admin/js/common.js
+++ admin/js/common.js
@@ -1,4 +1,24 @@
 $(function(){
+  $(".topArea_tab a").click(function(e){
+    e.preventDefault();
+    var tab = $(this).attr("data-role");
+    $(this).addClass("active").siblings().removeClass("active");
+    $(".tabArea").removeClass("active");
+    $("#"+tab).addClass("active");
+  });
 
+  $(".open--popup").click(function(e){
+    e.preventDefault();
+    var target = $(this).attr("data-pop");
+    $("#"+target).fadeIn(300);
+    dimOpen();
+  });
+
+  $(".popupBasic .btn button.close").click(function(){
+    $(this).parents(".popupBasic").fadeOut(300);
+    dimClose();
+  });
 });
 
+function dimOpen(){ $("#dim").fadeIn(300); }
+function dimClose(){ $("#dim").fadeOut(300); }
